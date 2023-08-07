d2fb4fa59a32f0f86a8189be5bf0b21a98334252
ckdev88 <ckdev88@outlook.com>
2023-08-07 10:32:29 -0300

    translate hackernoon comments

--- incl/functions.php
+++ incl/functions.php
@@ -206,16 +206,16 @@ function getArticle($url = false)
 	$html = str_replace('\\\\\n\u003e', '<p style="border-left:.5em solid yellow;padding-left:3em;font-size:1.25em;font-style:italic;">',  $html); // quote: goed
 	$html = str_replace('\\\n### ', '<p class="hekje-3x" style="font-weight:bold;font-size:16px;">', $html); // niet fout
 	$html = str_replace('### ', '<p class="hekje-3x" style="font-weight:bold;font-size:16px;">', $html); // niet fout
-	$html = str_replace('</p>\\\\\n', '</p><p>', $html); // ruimen
-	$html = str_replace('</p>\\\</p>', '</p><p>', $html); // ruimen
-	$html = str_replace('</p>\<p', '</p><p', $html); // ruimen
-	$html = str_replace('\n', '</p>', $html); // ruimen
-	$html = str_replace('![](', '<p style="display:none;">![](', $html); //verbergen
-	$html = str_replace('</p>##', '<p style="font-weight:bold;font-size:1.25em">', $html); //verbergen
-	$html = str_replace('\\\</p>', '', $html); //verbergen
-	$html = str_replace('</p>#<p', '</p><p', $html); //verbergen
-	$html = str_replace('</p>#<p', '</p><p', $html); //verbergen
-	$html = str_replace('</p><p>[   <p', '</p><p', $html); //verbergen
+	$html = str_replace('</p>\\\\\n', '</p><p>', $html); // cleanup
+	$html = str_replace('</p>\\\</p>', '</p><p>', $html); // cleanup
+	$html = str_replace('</p>\<p', '</p><p', $html); // cleanup
+	$html = str_replace('\n', '</p>', $html); // cleanup
+	$html = str_replace('![](', '<p style="display:none;">![](', $html); // hide
+	$html = str_replace('</p>##', '<p style="font-weight:bold;font-size:1.25em">', $html); // hide
+	$html = str_replace('\\\</p>', '', $html); // hide
+	$html = str_replace('</p>#<p', '</p><p', $html); // hide
+	$html = str_replace('</p>#<p', '</p><p', $html); // hide
+	$html = str_replace('</p><p>[   <p', '</p><p', $html); // hide
 
 	return $html;
 }
