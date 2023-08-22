d658b803d82d1fd66787a224febda73934f1c065
ckdev88 <ckdev88@outlook.com>
2023-08-21 23:58:32 -0300

    improve vim-style scrolling, no for cursor in input & textarea

--- incl/script.js
+++ incl/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('keydown', function(event) {
+        if (event.target.nodeName == 'INPUT' || event.target.nodeName == 'TEXTAREA') return false;
         if (event.key === 'j') { window.scrollBy(0, 50) }
         else if (event.key === 'k') { window.scrollBy(0, -50); }
 });
