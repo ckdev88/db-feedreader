5860e211bf998287360b97dcc02a8b3d09e23815
ckdev88 <ckdev88@outlook.com>
2023-08-22 08:18:44 -0300

    tweak vim-style scrolling

--- incl/script.js
+++ incl/script.js
@@ -1,5 +1,6 @@
-document.addEventListener('keydown', function(event) {
-        if (event.target.nodeName == 'INPUT' || event.target.nodeName == 'TEXTAREA') return false;
-        if (event.key === 'j') { window.scrollBy(0, 50) }
-        else if (event.key === 'k') { window.scrollBy(0, -50); }
+document.addEventListener('keypress', function(e) {
+        if (e.target.nodeName === 'INPUT' || e.target.nodeName === 'TEXTAREA' || e.target.nodeName === 'BUTTON' || e.target.nodeName === 'SELECT') return;
+        else if (e.key === 'k') window.scrollBy(0, -50);
+        else if (e.key === 'j') window.scrollBy(0, 50);
+        else return
 });
